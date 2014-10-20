35975b0a6cfe04d8956c8da93778a21f238c1acc
Pelle Wessman <pelle@kodfabrik.se>
2014-10-20 23:04:56 +0200

    Added comments to indie-config loader

--- js/indieconfig.js
+++ js/indieconfig.js
@@ -11,6 +11,7 @@ window.loadIndieConfig = (function () {
     callbacks = [],
     handleConfig, parseConfig;
 
+  // When the configuration has been loaded – deregister all loading mechanics and call all callbacks
   handleConfig = function () {
     config = config || {};
 
@@ -26,6 +27,7 @@ window.loadIndieConfig = (function () {
     }
   };
 
+  // When we receive a message, check if the source is right and try to parse it
   parseConfig = function (message) {
     var correctSource = (configFrame && message.source === configFrame.contentWindow);
 
@@ -39,25 +41,30 @@ window.loadIndieConfig = (function () {
   };
 
   return function (callback) {
+    // If the config is already loaded, call callback right away
     if (config) {
       callback(config);
       return;
     }
 
+    // Otherwise add the callback to the queue
     callbacks.push(callback);
 
+    // Are we already trying to load the Indie-Config, then wait
     if (configFrame) {
       return;
     }
 
+    // Create the iframe that will load the Indie-Config
     configFrame = document.createElement('iframe');
     configFrame.src = 'web+action:load';
     document.getElementsByTagName('body')[0].appendChild(configFrame);
     configFrame.style.display = 'none';
 
+    // Listen for messages so we will catch the Indie-Config message
     window.addEventListener('message', parseConfig);
 
-    // If no config has been loaded in 2 seconds, abort
+    // And if no such Indie-Config message has been loaded in a while, abort the loading
     configTimeout = setTimeout(handleConfig, 3000);
   };
 }());
